b335b00446949d0ad6a293ea3dfcf81c3957ea99
Fauzi Alvan <youhaveamessag3@gmail.com>
2022-02-18 15:39:32 +0700

    HomeSection

---
+++ src/components/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import imagebg from '../assets/bg.jpg'
+import { Card, Container } from 'react-bootstrap'
+
+
+const Home = () => {
+    return (
+        <div className="hero">
+            <Card className="bg-dark text-white border-0">
+                <Card.Img src={imagebg} alt="Card image" height='550px' />
+                <Card.ImgOverlay className='d-flex flex-column justify-content-center'>
+                    <Container>
+                        <Card.Title className='fs-1 fw-bolder display-3 mb-0'>New Season Arrival</Card.Title>
+                        <Card.Text className='fs-3 lead'>
+                            CHECK OUT ALL THE TREND
+                        </Card.Text>
+                    </Container>
+                </Card.ImgOverlay>
+            </Card>
+        </div>
+    )
+}
+
+export default Home
